7373c85a5d92d6f0c8aefd309bdddbe5d42e28bb
numbat70 <M00nbeamg>
2020-07-08 15:01:32 +0100

    mk
    mk

--- _posts/2020-01-01-Beta.mk
+++ _posts/2020-01-01-Beta.mk
@@ -4,7 +4,7 @@ title: Lab Research         # Title to show on the page
 type: lab                   # Chose from: lab, online, a11y, other, partner
 phase: beta                 # chose from discovery, alpha, beta, live
 initials: LH & MK           # initials of person who did/uploaded the research
-display_date: 22nd May 2020 # Date to show on the page
+display_date: 1st Jan 2020 # Date to show on the page
 return_url: "https://scotentsd.github.io/international/timeline/" # leave like this - don't change it   
 type_of_event: online       # add the type of event if you want it displayed added to the heading when the post is clicked on
                             # Lab Testing, Partner co-creation and co-design, Accessibility, Online research and testing, Events, F2F and testing
@@ -26,7 +26,7 @@ type_of_event: online       # add the type of event if you want it displayed add
 
 
 **What the test involved**
--
+- 
 
 
 **What we discovered**
